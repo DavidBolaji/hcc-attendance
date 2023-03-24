2e2f67c71fff4393d9d752363d6eae2eb9f2c9df
davidbolaji <odavidbolaji14@gmail.com>
2023-03-24 21:07:24 +0100

    remove required

--- src/controller/employeeController.js
+++ src/controller/employeeController.js
@@ -5,24 +5,26 @@ exports.register = async (req, res) => {
   try {
     const user = new Employee({ ...req.body });
     const newUser = await user.save();
-    const QRuser = await Employee.findByIdAndUpdate(newUser._id, {qr: process.env.FRONT_END_URL+'/'+newUser._id},{new:true})
+    const QRuser = await Employee.findByIdAndUpdate(
+      newUser._id,
+      { qr: process.env.FRONT_END_URL + "/" + newUser._id },
+      { new: true }
+    );
     const token = await QRuser.genAuthToken();
 
     await QRuser.save();
 
     res.status(201).send({ user, token });
   } catch (e) {
-    res.status(400).send({message: e});
+    res.status(400).send({ message: e });
   }
 };
 
 exports.login = async (req, res) => {
-  
- 
   try {
     const user = await Employee.validateUser(req.body.email, req.body.password);
-    if(user.err) {
-      console.log(user.err)
+    if (user.err) {
+      console.log(user.err);
       return res.status(400).send({ e: user.err });
     }
     const token = await user.genAuthToken();
@@ -33,13 +35,10 @@ exports.login = async (req, res) => {
 };
 
 exports.logout = async (req, res) => {
-
   try {
     req.user.tokens = req.user.tokens.filter((token) => {
       return req.token !== token.token;
     });
-
-    console.log(req.user.tokens)
 
     await req.user.save();
     res.send(req.user);
@@ -59,26 +58,26 @@ exports.logoutAll = async (req, res) => {
   }
 };
 
-
 // GET /users
 // Retrieve all users
 // Example: /users?page=1&limit=10&sort=name&search=John
 exports.getUsers = async (req, res) => {
-  const { page = 1, limit = 10, sort = 'createdAt', search = '' } = req.query;
+  const { page = 1, limit = 10, sort = "createdAt", search = "" } = req.query;
 
   const options = {
     page: parseInt(page, 10),
     limit: parseInt(limit, 10),
-    sort: { [sort]: 1 },
+    // sort: { [sort]: 1 },
+    // sort: { firstName: 1 },
     collation: {
-      locale: 'en',
+      locale: "en",
     },
   };
 
   const query = {
     $or: [
-      { name: { $regex: search, $options: 'i' } },
-      { email: { $regex: search, $options: 'i' } },
+      { name: { $regex: search, $options: "i" } },
+      { email: { $regex: search, $options: "i" } },
     ],
   };
 
@@ -87,54 +86,52 @@ exports.getUsers = async (req, res) => {
     res.json(users);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
 
-
-exports.getUserAll = async (req, res) => { 
+exports.getUserAll = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 10;
-  const sort = req.query.sort || 'createdAt';
-  const direction = req.query.direction || 'desc';
-  const filter = req.query.filter || '';
-  const end = req.query.end || '';
+  const sort = req.query.sort || "createdAt";
+  const direction = req.query.direction || "desc";
+  const filter = req.query.filter || "";
+  const end = req.query.end || "";
 
   const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-     try {
-      const startDate = new Date(filter);
+  const endIndex = page * limit;
+  try {
+    const startDate = new Date(filter);
 
-      const endDate =  new Date(end);
-        const totalCount = await Employee.find({}, '-password -tokens').count()
-        const users = await Employee.find({}, '-password -tokens')
-        .sort({ [sort]: direction })
-        .limit(limit)
-        .skip(startIndex)
-        .exec()
-        
-        const attendance = await Attendance.find({createdAt: {$gte: startDate, $lt: endDate }});
-        const usersWithAttendance = users.map((user) => {
-          const userAttendance = attendance.filter((att) => att.employeeId.toString() === user._id.toString());
-          return {
-            ...user.toObject(),
-            attendance: userAttendance,
-          };
-        })
-        
-        
-        res.status(200).send({data: usersWithAttendance, totalCount });
+    const endDate = new Date(end);
+    const totalCount = await Employee.find({}, "-password -tokens").count();
+    const users = await Employee.find({}, "-password -tokens")
+      .sort({ [sort]: direction })
+      // .sort({ [name]: 1 })
+      .limit(limit)
+      .skip(startIndex)
+      .exec();
 
-    } catch (e) {
-        res.status(400).send({ error: "User not found" });
-    }
-   
-  };
+    const attendance = await Attendance.find({
+      createdAt: { $gte: startDate, $lt: endDate },
+    });
+    const usersWithAttendance = users.map((user) => {
+      const userAttendance = attendance.filter(
+        (att) => att.employeeId.toString() === user._id.toString()
+      );
+      return {
+        ...user.toObject(),
+        attendance: userAttendance,
+      };
+    });
 
+    res.status(200).send({ data: usersWithAttendance, totalCount });
+  } catch (e) {
+    res.status(400).send({ error: "User not found" });
+  }
+};
 
-
-
-// exports.getUserAll = async (req, res) => { 
+// exports.getUserAll = async (req, res) => {
 //     try {
 //         const users = await Employee.find({}, '-password -tokens');
 //         const attendance = await Attendance.find();
@@ -165,7 +162,6 @@ exports.getUserAll2 = async (req, res) => {
   }
 };
 
-
 exports.getUser = async (req, res) => {
   const { id } = req.params;
 
@@ -176,22 +172,25 @@ exports.getUser = async (req, res) => {
       throw new Error();
     }
 
-    res.status(200).send(user)
+    res.status(200).send(user);
   } catch (error) {
     res.status(400).send({ error: "User not found" });
   }
 };
 
 exports.update = async (req, res) => {
-    const url = req.query.url;
-    try {
-      const user = await Employee.findOneAndUpdate({_id:req.query.url.split('=')[1] }, {qr: url},{new: true})
-      return res.status(200).send(user);
-  
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  };
+  const url = req.query.url;
+  try {
+    const user = await Employee.findOneAndUpdate(
+      { _id: req.query.url.split("=")[1] },
+      { qr: url },
+      { new: true }
+    );
+    return res.status(200).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
 // exports.update = async (req, res) => {
 //   try {
@@ -208,8 +207,6 @@ exports.update = async (req, res) => {
 //       res.status(400).send({ error: "Invalid Updates" });
 //     }
 
-
-
 //       await user[0].save();
 
 //       return res.status(200).send(user);
@@ -218,4 +215,3 @@ exports.update = async (req, res) => {
 //     res.status(400).send(e);
 //   }
 // };
-
--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -1,19 +1,20 @@
-const mongoose = require('../db/mongoose');
+const mongoose = require("../db/mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoosePaginate = require("mongoose-paginate-v2");
 
 // Define the employee schema
-const employeeSchema = new mongoose.Schema({
+const employeeSchema = new mongoose.Schema(
+  {
     firstName: {
       type: String,
     },
     lastName: {
-      type: String
+      type: String,
     },
     gender: {
-      type: String
+      type: String,
     },
     nbusStop: {
       type: String,
@@ -22,20 +23,20 @@ const employeeSchema = new mongoose.Schema({
       type: String,
     },
     email: {
-        type: String,
-        required: [true, "A user must have an email"],
-        trim: true,
-        lowercase: true,
-        unique: true,
-        // validate(value) {
-        //   if (!validator.isEmail(value)) {
-        //     throw new Error("Please enter a valid email!");
-        //   }
-        // },
-      },
+      type: String,
+      // required: [true, "A user must have an email"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      // validate(value) {
+      //   if (!validator.isEmail(value)) {
+      //     throw new Error("Please enter a valid email!");
+      //   }
+      // },
+    },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phone: {
       type: String,
@@ -43,97 +44,96 @@ const employeeSchema = new mongoose.Schema({
     address: {
       type: String,
     },
-    qr: {type: String, default: "https://hcc.com"},
+    qr: { type: String, default: "https://hcc.com" },
     DOB: {
       type: String,
     },
     month: {
-      type: String
+      type: String,
     },
     occupation: {
-      type:String
+      type: String,
     },
     role: {
-      type: ['member','worker','Admin']
-    }, 
-    tokens: [
-        {
-          token: {
-            type: String,
-            required: true,
-          },
-        },
-      ],
+      type: ["member", "worker", "Admin"],
     },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
   {
     timestamps: true,
-    strictPopulate: false
-  });
+    strictPopulate: false,
+  }
+);
 
+employeeSchema.methods.genAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
+employeeSchema.statics.validateUser = async (email, password) => {
+  const user = await Employee.findOne({ email });
 
-  employeeSchema.methods.genAuthToken = async function () {
-    const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET);
-    user.tokens = user.tokens.concat({ token });
-    await user.save();
-    return token;
-  };
-
-  employeeSchema.statics.validateUser = async (email, password) => {
-    const user = await Employee.findOne({ email });
-  
-    try {
-      if (!user) {
-       user.err = 'Unable to login'; 
-      }
-  
-      const isMatch = await bcrypt.compare(password, user.password);
-  
-      if (!isMatch) {
-       user.err = 'Wrong email or password';  
-      }
-
-      if(user.role[0] !== 'Admin') {
-        user.err = 'Unauthorized';
-      }
-    } catch (e) {
-      return e;
+  try {
+    if (!user) {
+      user.err = "Unable to login";
     }
-  
-    return user;
-  };
-  
-  employeeSchema.pre("save", async function (next) {
-    const user = this;
-  
-    if (user.isModified("password")) {
-      user.password = await bcrypt.hash(user.password, 8);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      user.err = "Wrong email or password";
     }
-  
-    next();
-  });
-  
-  employeeSchema.methods.toJSON = function () {
-    const user = this;
-    const userObject = user.toObject();
-  
-    delete userObject.password;
-    delete userObject.tokens;
-  
-    return userObject;
-  };
 
-  employeeSchema.virtual("attendances", {
-    ref: "Attendance",
-    localField: "_id",
-    foreignField: 'employeeId'
-  })
+    if (user.role[0] !== "Admin") {
+      user.err = "Unauthorized";
+    }
+  } catch (e) {
+    return e;
+  }
 
-  employeeSchema.plugin(mongoosePaginate);
+  return user;
+};
 
-  // Define the models
-const Employee = mongoose.model('Employee', employeeSchema);
+employeeSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+employeeSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+employeeSchema.virtual("attendances", {
+  ref: "Attendance",
+  localField: "_id",
+  foreignField: "employeeId",
+});
+
+employeeSchema.plugin(mongoosePaginate);
+
+// Define the models
+const Employee = mongoose.model("Employee", employeeSchema);
 
 // Export the models
 module.exports = Employee;
